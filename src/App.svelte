d0306ac2023856bddc003fbac9eeb81d5ce16029
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2020-09-05 06:41:37 -0400

    svelte: add a svelte+typescript project

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    export let name: string;
+</script>
+
+<style>
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
+
+    h1 {
+        color: #ff3e00;
+        text-transform: uppercase;
+        font-size: 4em;
+        font-weight: 100;
+    }
+
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
+</style>
+
+<main>
+    <h1>Hello {name}!</h1>
+    <p>Type something: <input bind:value={name} /></p>
+</main>
