85295e530a3933091d44af976dab232263df2a3b
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2020-09-05 07:56:17 -0400

    protos: add go support

--- cmd/jsso2/main.go
+++ cmd/jsso2/main.go
@@ -1,9 +1,16 @@
 package main
 
-import "github.com/jrockway/opinionated-server/server"
+import (
+	"github.com/jrockway/jsso2/pkg/foopb"
+	"github.com/jrockway/opinionated-server/server"
+	"google.golang.org/grpc"
+)
 
 func main() {
 	server.AppName = "jsso2"
+	server.AddService(func(s *grpc.Server) {
+		foopb.RegisterNameServiceService(s, &foopb.NameServiceService{})
+	})
 	server.Setup()
 	server.ListenAndServe()
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/jrockway/jsso2
 go 1.15
 
 require (
+	github.com/golang/protobuf v1.4.2
 	github.com/jrockway/opinionated-server v0.0.12
-	google.golang.org/protobuf v1.25.0 // indirect
+	google.golang.org/grpc v1.33.0-dev
+	google.golang.org/protobuf v1.25.0
 )
