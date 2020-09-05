044f21bb441077f2fa5e3b0bec6a3e06076f14f5
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2020-09-05 08:08:16 -0400

    main: add grpcui

--- cmd/jsso2/main.go
+++ cmd/jsso2/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
+	"net/http"
+
+	"github.com/fullstorydev/grpcui/standalone"
 	"github.com/jrockway/jsso2/pkg/foopb"
 	"github.com/jrockway/opinionated-server/server"
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
 )
 
@@ -10,6 +15,18 @@ func main() {
 	server.AppName = "jsso2"
 	server.AddService(func(s *grpc.Server) {
 		foopb.RegisterNameServiceService(s, &foopb.NameServiceService{})
+	})
+	server.SetStartupCallback(func(info server.Info) {
+		// This starts up grpcui on the debug port.
+		cc, err := grpc.Dial(info.GRPCAddress, grpc.WithInsecure())
+		if err != nil {
+			zap.L().Fatal("problem connecting to self", zap.Error(err))
+		}
+		h, err := standalone.HandlerViaReflection(context.Background(), cc, "self")
+		if err != nil {
+			zap.L().Fatal("problem creating grpcui handler", zap.Error(err))
+		}
+		http.Handle("/grpcui/", http.StripPrefix("/grpcui", h))
 	})
 	server.Setup()
 	server.ListenAndServe()
--- go.mod
+++ go.mod
@@ -3,8 +3,11 @@ module github.com/jrockway/jsso2
 go 1.15
 
 require (
+	github.com/fullstorydev/grpcui v1.0.0
+	github.com/fullstorydev/grpcurl v1.7.0 // indirect
 	github.com/golang/protobuf v1.4.2
 	github.com/jrockway/opinionated-server v0.0.12
+	go.uber.org/zap v1.15.0
 	google.golang.org/grpc v1.33.0-dev
 	google.golang.org/protobuf v1.25.0
 )
