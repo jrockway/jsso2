ef6744e464bc4cc6af25ddecaf27e3126fb5bd7d
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2020-09-05 09:46:04 -0400

    add "npm run all" to run the frontend and the backend at the same time

--- rollup.config.js
+++ rollup.config.js
@@ -18,10 +18,14 @@ function serve() {
     return {
         writeBundle() {
             if (server) return;
-            server = require("child_process").spawn("npm", ["run", "start", "--", "--dev"], {
-                stdio: ["ignore", "inherit", "inherit"],
-                shell: true,
-            });
+            server = require("child_process").spawn(
+                "npm",
+                ["run", "start", "--", "--dev", "--no-clear"],
+                {
+                    stdio: ["ignore", "inherit", "inherit"],
+                    shell: true,
+                }
+            );
 
             process.on("SIGTERM", toExit);
             process.on("exit", toExit);
