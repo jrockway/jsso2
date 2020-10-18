eae9600ebd8cc57f7329cb25b42e219610f9cecf
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2020-10-18 05:21:46 -0400

    jtesting: add the ability to get a grpc client

--- pkg/jtesting/jtesting.go
+++ pkg/jtesting/jtesting.go
@@ -6,6 +6,8 @@ import (
 	"crypto/md5"
 	"database/sql"
 	"fmt"
+	"math/rand"
+	"net"
 	"os"
 	"path/filepath"
 	"runtime"
@@ -19,6 +21,7 @@ import (
 	"github.com/joho/godotenv"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zaptest"
+	"google.golang.org/grpc"
 )
 
 // Config contains test-specific configuration.
@@ -31,25 +34,25 @@ type R struct {
 	Timeout  time.Duration
 	Logger   bool
 	Database bool
+	GRPC     func(t *testing.T, e *E, s *grpc.Server)
 }
 
 // E holds per-test "extras".
 type E struct {
-	Context context.Context
-	Logger  *zap.Logger
-	Config  *Config
-	DSN     string
-	DB      *sql.DB
+	Context    context.Context
+	Logger     *zap.Logger
+	Config     *Config
+	DSN        string
+	DB         *sql.DB
+	ClientConn *grpc.ClientConn
 }
 
 // Run runs the provided test function as a subtest with the desired Extras available.
 func Run(t *testing.T, name string, r R, f func(t *testing.T, e *E)) {
 	t.Helper()
-	pc, file, _, pcOk := runtime.Caller(1)
+	pc, _, _, pcOk := runtime.Caller(1)
 	t.Run(name, func(t *testing.T) {
-		var ctx context.Context
-		var c func()
-
+		_, file, _, _ := runtime.Caller(0)
 		envFile := filepath.Clean(filepath.Join(file, "..", "..", "..", "env.test"))
 		if err := godotenv.Load(envFile); err != nil {
 			t.Fatalf("failed to load %s: %v", envFile, err)
@@ -60,11 +63,14 @@ func Run(t *testing.T, name string, r R, f func(t *testing.T, e *E)) {
 				SuperuserDSN: os.Getenv("SUPERUSER_DATABASE_URL"),
 			},
 		}
+
+		ctx, c := context.WithCancel(context.Background())
 		if r.Timeout > 0 {
+			c()
 			ctx, c = context.WithTimeout(context.Background(), r.Timeout)
-		} else {
-			ctx, c = context.WithCancel(context.Background())
 		}
+		defer c()
+
 		if r.Logger {
 			logger := zaptest.NewLogger(t, zaptest.Level(zap.DebugLevel))
 			defer logger.Sync()
@@ -73,6 +79,8 @@ func Run(t *testing.T, name string, r R, f func(t *testing.T, e *E)) {
 			extras.Logger = logger.Named("test." + name)
 			ctx = ctxzap.ToContext(ctx, logger)
 		}
+		extras.Context = ctx
+
 		if r.Database {
 			if !pcOk {
 				t.Fatal("could not determine caller to generate database name")
@@ -88,14 +96,34 @@ func Run(t *testing.T, name string, r R, f func(t *testing.T, e *E)) {
 			}
 			extras.DB = db
 		}
-		extras.Context = ctx
+		if r.GRPC != nil {
+			gen := rand.New(rand.NewSource(time.Now().UnixNano()))
+			name := filepath.Join(os.TempDir(), fmt.Sprintf("jsso-grpc-%d", gen.Int()))
+			l, err := net.Listen("unix", name)
+			if err != nil {
+				t.Fatalf("listen on unix file %s: %v", name, err)
+			}
+			defer l.Close()
+			defer os.Remove(name)
+
+			s := grpc.NewServer()
+			r.GRPC(t, extras, s)
+			go s.Serve(l)
+			defer s.Stop()
+
+			cc, err := grpc.DialContext(ctx, "unix:///"+name, grpc.WithInsecure())
+			if err != nil {
+				t.Fatalf("dial grpc server: %v", err)
+			}
+			extras.ClientConn = cc
+			defer cc.Close()
+		}
 		f(t, extras)
 		select {
 		case <-ctx.Done():
 			t.Fatalf("after tests: %v", ctx.Err())
 		default:
 		}
-		c()
 	})
 }
 
