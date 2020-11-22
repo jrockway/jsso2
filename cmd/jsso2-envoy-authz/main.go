c9a4675c613410d8f38a1c8485385746a0e0b0b0
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2020-11-21 19:12:44 -0500

    envoy-authz: implement server

--- cmd/jsso2-envoy-authz/main.go
+++ cmd/jsso2-envoy-authz/main.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	envoy_auth "github.com/envoyproxy/go-control-plane/envoy/service/auth/v3"
+	"github.com/jrockway/jsso2/pkg/envoyauthz"
 	"github.com/jrockway/opinionated-server/server"
 	"google.golang.org/grpc"
 )
 
 func main() {
 	server.AppName = "jsso2-envoy-proxy"
+	authzCfg := &envoyauthz.Config{}
+	server.AddFlagGroup("Authorization Server", authzCfg)
 	server.Setup()
+	svc := &envoyauthz.Service{}
 	server.AddService(func(s *grpc.Server) {
-		envoy_auth.RegisterAuthorizationServer(s, &envoy_auth.UnimplementedAuthorizationServer{})
+		envoy_auth.RegisterAuthorizationServer(s, svc)
 	})
 	server.ListenAndServe()
 }
