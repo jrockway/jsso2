a41cd05d278299ec9f0fe6409162c4fb92d4841e
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2020-11-26 13:48:05 -0500

    jsso2-protected-example: return html instead of text

--- cmd/jsso2-protected-example/main.go
+++ cmd/jsso2-protected-example/main.go
@@ -10,14 +10,16 @@ func main() {
 	server.AppName = "jsso2-protected-example"
 	server.Setup()
 	server.SetHTTPHandler(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Add("content-type", "text/plain")
+		w.Header().Add("content-type", "text/html")
 		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("<html><head><title>Example Protected App</title></head><body><p>"))
 		if username := req.Header.Get("x-jsso2-username"); username != "" {
 			w.Write([]byte("Logged in as "))
 			w.Write([]byte(username))
 		} else {
-			w.Write([]byte("ok"))
+			w.Write([]byte("Not logged in"))
 		}
+		w.Write([]byte("</p></body></html>"))
 	}))
 	server.ListenAndServe()
 }
