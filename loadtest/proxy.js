a2600aeeb6a4300bf322a290789f2ca7e7156bab
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2020-11-28 04:05:23 -0500

    add a load test

---
+++ loadtest/proxy.js
@@ -0,0 +1,10 @@
+import http from "k6/http";
+import { check } from "k6";
+export default function () {
+    const res = http.get("http://localhost:4000/protected", {
+        redirects: 0,
+    });
+    check(res, {
+        "response code was 307": (res) => res.status == 307,
+    });
+}
