ea8892e73020985aa45f04ee71db341c8fa1cddc
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2020-11-29 22:26:19 -0500

    store: add tracing and monitoring to DoTx

--- go.mod
+++ go.mod
@@ -22,6 +22,8 @@ require (
 	github.com/mattn/go-sqlite3 v1.10.0 // indirect
 	github.com/o1egl/paseto/v2 v2.1.1
 	github.com/olekukonko/tablewriter v0.0.4
+	github.com/opentracing/opentracing-go v1.2.0
+	github.com/prometheus/client_golang v1.7.1
 	github.com/spf13/cobra v1.0.0
 	github.com/spf13/pflag v1.0.3
 	github.com/spf13/viper v1.4.0
--- pkg/store/tx.go
+++ pkg/store/tx.go
@@ -9,7 +9,22 @@ import (
 	"time"
 
 	"github.com/jmoiron/sqlx"
+	"github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/ext"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"go.uber.org/zap"
+)
+
+var (
+	txStarted = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "jsso2_db_tx_started",
+		Help: "Number of attempts to begin a transaction via DoTx.",
+	})
+	txFinished = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name: "jsso2_db_tx_finished",
+		Help: "Number of transactions started via DoTx that finished.",
+	}, []string{"status"})
 )
 
 // Retryable allows you to explicitly mark an error as retryable.
@@ -47,8 +62,10 @@ func isRetryable(err error) bool {
 // DoTx executes the provied function in a transaction, retrying it if it rolls back.  You should
 // not manually commit or roll back the provided transaction; return an error to roll back or return
 // nil to commit.
-func (c *Connection) DoTx(ctx context.Context, l *zap.Logger, readOnly bool, f func(tx *sqlx.Tx) error) error {
+func (c *Connection) DoTx(origCtx context.Context, l *zap.Logger, readOnly bool, f func(tx *sqlx.Tx) error) error {
+	span, ctx := opentracing.StartSpanFromContext(origCtx, "do_tx")
 	var errs []error
+	defer span.Finish()
 	defer func() {
 		if len(errs) > 0 {
 			// We only log this to avoid suppressing errors that might be interesting to
@@ -58,15 +75,18 @@ func (c *Connection) DoTx(ctx context.Context, l *zap.Logger, readOnly bool, f f
 		}
 	}()
 	for i := 0; i < MaxRetries; i++ {
+		span.LogKV("attempt", i)
 		if i != 0 {
 			l.Debug("DoTx: retrying transaction after a delay", zap.Int("attempt", i), zap.Int("max_attempts", MaxRetries), zap.Errors("errors", errs), zap.Duration("delay", TxDelay))
 			time.Sleep(TxDelay)
 		}
+		txStarted.Inc()
 		tx, err := c.db.BeginTxx(ctx, &sql.TxOptions{
 			Isolation: sql.LevelSerializable,
 			ReadOnly:  readOnly,
 		})
 		if err != nil {
+			txFinished.WithLabelValues("failed_start").Inc()
 			if isRetryable(err) {
 				errs = append(errs, fmt.Errorf("attempt %d: begin tx: %w", i, err))
 				continue
@@ -76,9 +96,11 @@ func (c *Connection) DoTx(ctx context.Context, l *zap.Logger, readOnly bool, f f
 		if err := f(tx); err != nil {
 			if rErr := tx.Rollback(); rErr != nil {
 				if !errors.Is(rErr, sql.ErrTxDone) {
+					txFinished.WithLabelValues("failed_rollback").Inc()
 					return fmt.Errorf("attempt %d: error in user function (%v), but rollback failed too: %w", i, err, rErr)
 				}
 			}
+			txFinished.WithLabelValues("rollback").Inc()
 			if isRetryable(err) {
 				errs = append(errs, fmt.Errorf("attempt %d: user function: %w", i, err))
 				continue
@@ -86,13 +108,16 @@ func (c *Connection) DoTx(ctx context.Context, l *zap.Logger, readOnly bool, f f
 			return fmt.Errorf("attempt %d: user function: non-retryable error: %w", i, err)
 		}
 		if err := tx.Commit(); err != nil {
+			txFinished.WithLabelValues("failed_commit").Inc()
 			if isRetryable(err) {
 				errs = append(errs, fmt.Errorf("attempt %d: commit: %w", i, err))
 				continue
 			}
 		}
+		txFinished.WithLabelValues("commit").Inc()
 		return nil
 	}
+	ext.Error.Set(span, true)
 	if n := len(errs); n == 0 {
 		l.Panic("DoTx: no errors, but loop ended; that's impossible")
 		// Go doesn't know that this panics, so we pretend to return an error.
